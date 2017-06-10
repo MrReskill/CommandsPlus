e64336b083decbf1997dad532f7617d76e61dafb
Alban Weill <mrreskill@gmail.com>
2017-06-10 22:21:04 +0200

    Add FlyCommands.php

---
+++ src/CommandsPlus/Commands/Moderator/FlyCommands.php
@@ -0,0 +1,26 @@
+<?php
+namespace CommandsPlus\Commands\Moderation;
+use pocketmine\command\{Command, CommandSender};
+use CommandsPlus\Main;
+class FlyCommands extends Command
+{
+    
+    public function __construct()
+    {
+        parent::__construct("fly", "Use fly on your server", "/fly <player>");
+        $this->setPermission("commandsplus.command.fly");
+    }
+    
+    public function execute(CommandSender $sender, $currentAlias, array $args)
+    {
+        if(!$this->testPermission($sender)) return false;
+        if($sender->getAllowFlight() == true){
+          $sender->setAllowFlight(false);
+          $sender->sendMessage("§6Vous venez de désactivé le §efly");
+        } else {
+          $sender->setAllowFlight(true);
+          $sender->sendMessage("§2Vous venez d'activé le §afly");     
+        }
+        return true;
+    }
+}
